427dca69d839803251ef3955d1eabe1f44574fe3
ddb1494 <ddb1494@gmail.com>
2018-10-18 00:56:36 +0900

    a

--- css-tip/main.js
+++ css-tip/main.js
@@ -2,13 +2,6 @@ let app=document.getElementById('app');
 
 let tip1 = app.children[0];
 
-tip1.setAttribute('data-tip',`tip11
-content
-content
-content
-<b>Bold</b>`);
-
-
 Array.from(app.children).forEach((e,i)=>{
     e.textContent=`文档${i+1}\n`.repeat(4);
     e.setAttribute('data-tip', e.getAttribute('data-tip')+`\n.........`.repeat(2));
