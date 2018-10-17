033b75d0a14089d4c60ae81ba84e4aa5bec84a89
ddb1494 <ddb1494@gmail.com>
2018-10-17 22:59:11 +0900

    tip

---
+++ css-tip/main.js
@@ -0,0 +1,20 @@
+let app=document.getElementById('app');
+
+let tip1 = app.children[0];
+
+tip1.setAttribute('data-tip',`标题
+内容行1
+内容行2
+内容行3
+<b>Bold</b>`);
+
+
+
+app.children[3].textContent=`4
+4
+4
+4
+4
+4
+4
+`
